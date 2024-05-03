2627456a6f230cf6787e278b6e782f46d1d09675
cekator3 <hamsterdreams@inbox.ru>
2024-05-03 15:59:08 +0500

    ...

--- lib/repositories/favorite_place_repository/DTO/favorite_place_list_item.dart
+++ lib/repositories/favorite_place_repository/DTO/favorite_place_list_item.dart
@@ -2,5 +2,18 @@
 /// needed to generate a list of user's favorite geographic places.
 class FavoritePlaceListItem
 {
-  // ...
+  final String _id;
+  final String _name;
+  final String _address;
+
+  FavoritePlaceListItem({required String id, required String name, required String address}) : _id = id, _name = name, _address = address;
+
+  /// Returns geographic place's identifier
+  String getId() => _id;
+
+  /// Returns geographic place's name
+  String getName() => _name;
+
+  /// Returns geographic place's address
+  String getAddress() => _address;
 }
